05bf15e98e7bdfc0be1dcab1261905ec9ae5a9b8
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-04-10 17:53:47 +0800

    Learned about differences between Stateless and Stateful widgets

--- flutter_quiz/lib/main.dart
+++ flutter_quiz/lib/main.dart
@@ -2,9 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIdx = 0;
+
+  var question = [
+    "Question 1",
+    "Question 2",
+  ];
+
+  void answerQuestion() {
+    setState(() {
+      questionIdx += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello-ing'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+        body: Column(
+          children: [
+            Text(question[questionIdx]),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text("Answer 2"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
