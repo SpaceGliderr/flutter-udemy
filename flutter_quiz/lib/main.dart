62487fe1bb84e58a39da09865ccfa560283315f5
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-04-11 00:56:44 +0800

    Finish module 1 of the course

--- flutter_quiz/lib/main.dart
+++ flutter_quiz/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz/answer.dart';
-import 'package:flutter_quiz/question.dart';
+import 'package:flutter_quiz/quiz.dart';
+import 'package:flutter_quiz/result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +13,46 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIdx = 0;
+  var _totalScore = 0;
 
-  var question = [
-    "Question 1",
-    "Question 2",
+  final _questions = [
+    {
+      "questionText": "What is your favourite colour?",
+      "answers": [
+        {"text": "Red", "score": 10},
+        {"text": "Green", "score": 5},
+        {"text": "Blue", "score": 7}
+      ]
+    },
+    {
+      "questionText": "What is your favourite animal?",
+      "answers": [
+        {"text": "Dog", "score": 10},
+        {"text": "Cat", "score": 8},
+        {"text": "Parrot", "score": 5},
+        {"text": "Lion", "score": 2}
+      ]
+    },
+    {
+      "questionText": "What is your favourite food?",
+      "answers": [
+        {"text": "Nasi Lemak", "score": 8},
+        {"text": "Burgers", "score": 10}
+      ]
+    },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIdx += 1;
+    });
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIdx = 0;
+      _totalScore = 0;
     });
   }
 
@@ -32,14 +63,9 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("First App"),
         ),
-        body: Column(
-          children: [
-            Question(question[_questionIdx]),
-            Answer("Answer 1", _answerQuestion),
-            Answer("Answer 2", _answerQuestion),
-            Answer("Answer 3", _answerQuestion),
-          ],
-        ),
+        body: _questionIdx < _questions.length
+            ? Quiz(_questions, _questionIdx, _answerQuestion)
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
