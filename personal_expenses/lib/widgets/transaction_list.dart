55031a83986e74545e5b7ec722e8f642f41e35c1
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-04-12 01:59:14 +0800

    Switch to ListTile

--- personal_expenses/lib/widgets/transaction_list.dart
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -33,43 +33,26 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, idx) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "RM ${transactions[idx].amount.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(8),
+                        child: FittedBox(
+                          child: Text(
+                              "RM ${transactions[idx].amount.toStringAsFixed(2)}"),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[idx].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[idx].date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
+                    title: Text(
+                      transactions[idx].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[idx].date),
+                    ),
                   ),
                 );
               },
